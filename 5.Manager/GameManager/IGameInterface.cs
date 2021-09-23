99b43f883105cd79a922380c769acb5e3d191e9c
DevHwangIT <84306704+JackHwang-aloha@users.noreply.github.com>
2021-09-23 22:19:12 +0900

    Mobile Directory Add

---
+++ 5.Manager/GameManager/IGameInterface.cs
@@ -0,0 +1,34 @@
+﻿namespace MyLibrary.Manager
+{
+    enum GameState
+    {
+        Play,
+        Pause,
+        Resume,
+        End,
+        None
+    }
+
+    enum InGameState
+    {
+        Win,
+        Lose,
+        Draw,
+        None
+    }
+
+    public interface IGameState
+    {
+        void GamePlayCallBack();
+        void GamePauseCallBack();
+        void GameResumeCallBack();
+        void GameEndCallBack();
+    }
+
+    public interface IInGameState
+    {
+        void InGameWinCallback();
+        void InGameLoseCallback();
+        void InGameDrawCallback();
+    }
+}
