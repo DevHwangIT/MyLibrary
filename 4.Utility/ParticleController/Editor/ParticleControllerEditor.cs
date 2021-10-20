5888b56a7c316a690dc76b740c0932342a4664ee
Dev.Hwang <zxc1063@naver.com>
2021-10-20 16:12:25 +0900

    ParticleSystem Work

---
+++ 4.Utility/ParticleController/Editor/ParticleControllerEditor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MyLibrary.Utility;
+using UnityEditor;
+using UnityEngine;
+
+namespace MyLibrary.Utility
+{
+    [CustomEditor(typeof(ParticleController))]
+    public class ParticleControllerEditor : Editor
+    {
+        private ParticleController _controller;
+
+        private void OnEnable()
+        {
+            _controller = (ParticleController) target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            EditorGUILayout.Space(5);
+            EditorGUILayout.LabelField("Particle Property");
+            
+            _controller.HideType = (HideType) EditorGUILayout.EnumPopup("Hide Type : ", _controller.HideType);
+            if (_controller.HideType == HideType.ObjectPooling)
+            {
+                EditorGUILayout.Space(5);
+                SerializedProperty poolingCallEvent = serializedObject.FindProperty("onObjectPoolCalling");
+                EditorGUIUtility.LookLikeControls();
+                EditorGUILayout.PropertyField(poolingCallEvent);
+                serializedObject.ApplyModifiedProperties();
+            }
+            
+            _controller.destroyType = (DestroyCondition) EditorGUILayout.EnumPopup("Destory Type : ", _controller.destroyType);
+            EditorGUILayout.Space(5);
+            switch (_controller.destroyType)
+            {
+                case DestroyCondition.Distance:
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField("Distance Compare Target : ");
+                    _controller.distanceCompareTarget = (Transform) EditorGUILayout.ObjectField(_controller.distanceCompareTarget, typeof(Transform));
+                    EditorGUILayout.EndHorizontal();
+                    _controller.distanceValue = EditorGUILayout.FloatField("Destory Condition Distance : ", _controller.distanceValue);
+                    break;
+
+                case DestroyCondition.Time:
+                    _controller.durationTime = EditorGUILayout.FloatField("Destory Condition Destruction Time : ", _controller.durationTime);
+                    break;
+            }
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
---
+++ 4.Utility/ParticleController/ParticleController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Events;
+
+namespace MyLibrary.Utility
+{
+	public enum HideType
+	{
+		Destroy,
+		Scaling,
+		Enable,
+		ObjectPooling
+	}
+
+	public enum DestroyCondition
+	{
+		Distance,
+		Time,
+		DeleteCall
+	}
+	
+	public class ParticleController : MonoBehaviour
+	{
+		private HideType hideType = HideType.Enable;
+		public HideType HideType
+		{
+			get { return hideType; }
+			set
+			{
+				hideType = value;
+				if (hideType == HideType.Scaling)
+				{
+					for (int i = 0; i < particleSystems.Length; i++)
+					{
+						ParticleSystem.MainModule particle = particleSystems[i].main;
+						particle.scalingMode = ParticleSystemScalingMode.Hierarchy;
+					}
+				}
+				else
+				{
+					for (int i = 0; i < particleSystems.Length; i++)
+					{
+						ParticleSystem.MainModule particle = particleSystems[i].main;
+						particle.scalingMode = ParticleSystemScalingMode.Local;
+					}
+				}
+			}
+		}
+		public DestroyCondition destroyType = DestroyCondition.DeleteCall;
+
+		public Transform distanceCompareTarget = null;
+		public float distanceValue = 0;
+		private float destructionTimer = 0f;
+		public float durationTime = 0;
+
+		private ParticleSystem[] particleSystems;
+		private bool IsPaused = true;
+		private Vector3 InitPos;
+
+		public UnityEvent onObjectPoolCalling;
+		void Awake()
+		{
+			particleSystems = this.GetComponentsInChildren<ParticleSystem>(true);
+			Initialize();
+		}
+		
+		void FixedUpdate()
+		{
+			destructionTimer += Time.fixedDeltaTime;
+			switch (destroyType)
+			{
+				case DestroyCondition.Distance:
+					if (Vector3.Distance(InitPos, distanceCompareTarget.position) > distanceValue)
+						Stop();
+					break;
+				
+				case DestroyCondition.Time:
+					if (destructionTimer > durationTime)
+						Stop();
+					break;
+			}
+		}
+
+		private void Initialize()
+		{
+			InitPos = transform.position;
+			destructionTimer = 0f;
+			foreach (var particle in particleSystems)
+			{
+				particle.gameObject.transform.localScale = Vector3.one;
+				if (particle.isPlaying)
+					particle.Stop();
+			}
+			this.gameObject.SetActive(true);
+		}
+
+		public void Play()
+		{
+			Initialize();
+			foreach (var particle in particleSystems)
+			{
+				if (!particle.isPlaying) 
+					particle.Play();
+			}
+		}
+		
+		public void Pause()
+		{
+			foreach (var particle in particleSystems)
+			{
+				if (particle.isPlaying)
+					particle.Pause();
+			}
+		}
+
+		public void Stop()
+		{
+			foreach (var particle in particleSystems)
+			{
+				if (particle.isPlaying)
+					particle.Stop();
+			}
+			
+			switch (hideType)
+			{
+				case HideType.Destroy:
+					Destroy(this.gameObject);
+					break;
+				
+				case HideType.Scaling:
+					for (int i = 0; i < particleSystems.Length; i++)
+					{
+						particleSystems[i].gameObject.transform.localScale = Vector3.zero;
+					}
+					break;
+				
+				case HideType.Enable:
+					this.gameObject.SetActive(false);
+					break;
+				
+				case HideType.ObjectPooling:
+					onObjectPoolCalling?.Invoke();
+					break;
+			}
+		}
+	}
+}
