782c609cbbe601c76fbab18bf291dbbe99f8a190
Dev.Hwang <zxc1063@naver.com>
2021-10-24 07:53:23 +0900

    DataManager Add Singleton

--- GameTemplate/Manager/DataManager/DataManager.cs
+++ GameTemplate/Manager/DataManager/DataManager.cs
@@ -3,7 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public partial class DataManager : MonoBehaviour
-{
+{  
+    #region Singleton
+    private static DataManager _instance;
+    public static DataManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (DataManager) FindObjectOfType(typeof(DataManager));
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject($"{typeof(DataManager)} (Singleton)");
+                    _instance = singletonObject.AddComponent<DataManager>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+    
     static PlayerData PlayerData = new PlayerData();
     static UserData UserData = new UserData();
 }
