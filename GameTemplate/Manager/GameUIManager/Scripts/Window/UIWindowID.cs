6cf640472ec76fefdc57b061edd96231bb14419a
Dev.Hwang <zxc1063@naver.com>
2021-10-24 14:50:41 +0900

    WindowID ReadMe Add

--- GameTemplate/Manager/GameUIManager/Scripts/Window/UIWindowID.cs
+++ GameTemplate/Manager/GameUIManager/Scripts/Window/UIWindowID.cs
@@ -1,4 +1,9 @@
 
+/*
+ * ReadMe
+ *  if you want to use this UIWindow Template. you must this UIWindow.cs file move to out MyLibrary directory
+ */
+
 public enum UIWindowID : int
 {
     //Index is Draw Order
