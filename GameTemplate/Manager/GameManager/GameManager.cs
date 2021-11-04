2075204bf1891d2ebbb088e38b50f3f4b72098c8
Dev.Hwang <zxc1063@naver.com>
2021-11-04 10:48:08 +0900

    Sample Modify

--- GameTemplate/Manager/GameManager/GameManager.cs
+++ GameTemplate/Manager/GameManager/GameManager.cs
@@ -20,7 +20,6 @@ public partial class GameManager : MonoBehaviour
                     DontDestroyOnLoad(singletonObject);
                 }
             }
-
             return _instance;
         }
     }
