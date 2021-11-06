e8ca916ad651a28c2b2b828e82d3d97b85cf2fc5
Dev.Hwang <zxc1063@naver.com>
2021-11-07 08:37:59 +0900

    Add Window ID

---
+++ GameTemplate/Manager/GameUIManager/Scripts/UIWindowID.cs
@@ -0,0 +1,27 @@
+
+/*
+ * ReadMe
+ *  if you want to use this UIWindow Template. you must this UIWindow.cs file move to out MyLibrary directory
+ */
+
+public enum UIWindowID : int
+{
+    //Index is Draw Order
+    None,
+    
+    //LogIn
+    LogIn,
+    SignUp,
+    
+    //Lobby
+    
+    //Loading
+    SoloBattle,
+    TeamBattle,
+    
+    //InGame
+    
+    //Public
+    Setting,
+    GameMenu
+}
