76def70b064a7df443d6f21645de3a665f5164b1
Dev.Hwang <zxc1063@naver.com>
2021-11-14 11:38:08 +0900

    FSM Modify

--- 1.DesignPattern/FSM Pattern/Sample/FSMContollerExample.cs
+++ 1.DesignPattern/FSM Pattern/Sample/FSMContollerExample.cs
@@ -1,39 +1,42 @@
 ﻿using MyLibrary.DesignPattern.Sample;
 using UnityEngine;
 
-[System.Serializable]
-public enum StateType
+namespace MyLibrary.DesignPattern.Sample
 {
-    Idle,
-    Run,
-    Walk,
-    Jump
-}
-
-public class FSMContollerExample : MonoBehaviour
-{
-    [SerializeField] private StateType MotionType;
-    [SerializeField] private FSMExampleCharacter characterScript;
-    
-    private void Update()
+    [System.Serializable]
+    public enum StateType
     {
-        switch (MotionType)
+        Idle,
+        Run,
+        Walk,
+        Jump
+    }
+
+    public class FSMContollerExample : MonoBehaviour
+    {
+        [SerializeField] private StateType MotionType;
+        [SerializeField] private FSMExampleCharacter characterScript;
+
+        private void Update()
         {
-            case StateType.Idle:
-                characterScript.ChangeFSM = new FSMExampleCharacter.IdleState();
-                break;
-            
-            case StateType.Run:
-                characterScript.ChangeFSM = new FSMExampleCharacter.RunState();
-                break;
-            
-            case StateType.Jump:
-                characterScript.ChangeFSM = new FSMExampleCharacter.JumpState();
-                break;
-            
-            case StateType.Walk:
-                characterScript.ChangeFSM = new FSMExampleCharacter.WalkState();
-                break;
+            switch (MotionType)
+            {
+                case StateType.Idle:
+                    characterScript.ChangeFSM = new FSMExampleCharacter.IdleState();
+                    break;
+
+                case StateType.Run:
+                    characterScript.ChangeFSM = new FSMExampleCharacter.RunState();
+                    break;
+
+                case StateType.Jump:
+                    characterScript.ChangeFSM = new FSMExampleCharacter.JumpState();
+                    break;
+
+                case StateType.Walk:
+                    characterScript.ChangeFSM = new FSMExampleCharacter.WalkState();
+                    break;
+            }
         }
     }
 }
