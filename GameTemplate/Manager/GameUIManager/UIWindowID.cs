2e68e9192e86481ac633d0c34e1748d306fb4c7c
Dev.Hwang <zxc1063@naver.com>
2021-11-20 08:49:45 +0900

    UIWindow Add

---
+++ GameTemplate/Manager/GameUIManager/UIWindowID.cs
@@ -0,0 +1,12 @@
+
+/*
+ * ReadMe
+ *  if you want to use this UIWindow Template. you must this UIWindow.cs file move to out MyLibrary directory
+ */
+
+public enum UIWindowID : int
+{
+    //Index is Draw Order
+    None = 0,
+    GameMenu
+}
